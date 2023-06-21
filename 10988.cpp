03fdc682cfcc7679927981299fe0d2cd6c24560e
minsik kim <triton0305@gmail.com>
2023-06-21 15:32:01 +0900

    TIL_C(23_06_21)
    TIL_C(23_06_21)

---
+++ 10988.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+	char str[101]; int N = 0; int result = 0;
+	scanf("%s", str);
+	int k = 0;
+	while (str [k] != NULL) {
+		N++;
+		k++;
+	}
+	for (int i = 0; i < (N / 2); i++) {
+		if (str[i] == str[N - i - 1]) { result++; }
+		else { break; }
+	}
+	if ((N / 2) == result) { printf("%d", 1); }
+	else { printf("%d", 0); }
+	return 0;
+}
---
+++ 2444.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	int N;
+	scanf("%d", &N);
+	for (int i = 1; i < 2*N; i++) {
+		if (i < N) {
+			for (int j = 0; j < N-i; j++) printf(" ");
+			for (int k = 1; k < (2*i); k++) printf("*");
+		}
+		else if (i == N) for (int j = 1; j < 2 * N; j++) {
+			printf("*");
+		}
+		else {
+			for (int k = 0; k < i-N; k++) printf(" ");
+			for (int j = 1; j < 2*(2*N-i); j++) printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
